18c8529dad9d222463b004a10673267f946091d7
Geoff Buesing <gbuesing@gmail.com>
2014-07-09 18:12:17 -0500

    Initial import

---
+++ cta.py
@@ -0,0 +1,55 @@
+"""
+Script for pulling train times from CTA API for a specific stop ID,
+for displaying via Arduino Yun on an LCD.
+
+Usage:
+
+  python cta.py apikey stopid delimeter
+
+e.g:
+  
+  python cta.py abc123 12345 ','
+
+Necesary to first install python-expat on Yun -- SSH in and run:
+  opkg update
+  opkg install python-expat
+"""
+import sys
+import datetime
+import urllib2
+import xml.etree.ElementTree as ET
+
+
+API_KEY = sys.argv[1]
+STOP_ID = sys.argv[2]
+DELIMETER = sys.argv[3]
+
+def get_xml_from_api():
+  api_url = "http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=" + API_KEY + "&stpid=" + STOP_ID
+  return urllib2.urlopen(api_url).read()
+
+def extract_minutes_from_xml(xml):
+  tree = ET.fromstring(xml)
+  out = []
+  for elem in tree.findall('eta/arrT'):
+    datestr = elem.text
+    minutes = get_minutes_from_now(datestr)
+    out.append(minutes)
+  return out
+
+def get_minutes_from_now(datestr):
+  now = datetime.datetime.now()
+  obj = datetime.datetime.strptime(datestr, '%Y%m%d %H:%M:%S')
+  delta = obj - now
+  minutes = delta.total_seconds() / 60
+  return int(round(minutes))
+
+
+xml = get_xml_from_api()
+# xml = '''<?xml version="1.0" encoding="utf-8"?><ctatt><tmst>20140708 20:16:54</tmst><errCd>0</errCd><errNm /><eta><staId>41460</staId><stpId>30282</stpId><staNm>Irving Park</staNm><stpDe>Service toward Loop</stpDe><rn>429</rn><rt>Brn</rt><destSt>30249</destSt><destNm>Loop</destNm><trDr>5</trDr><prdt>20140708 20:15:55</prdt><arrT>20140708 20:20:55</arrT><isApp>0</isApp><isSch>0</isSch><isDly>0</isDly><isFlt>0</isFlt><flags /><lat>41.96621</lat><lon>-87.6941</lon><heading>89</heading></eta><eta><staId>41460</staId><stpId>30282</stpId><staNm>Irving Park</staNm><stpDe>Service toward Loop</stpDe><rn>424</rn><rt>Brn</rt><destSt>0</destSt><destNm>Loop</destNm><trDr>5</trDr><prdt>20140708 20:16:42</prdt><arrT>20140708 20:29:42</arrT><isApp>0</isApp><isSch>1</isSch><isDly>0</isDly><isFlt>0</isFlt><flags /><lat /><lon /><heading /></eta></ctatt>'''
+# print(xml)
+minutes = extract_minutes_from_xml(xml)
+minutes_str = DELIMETER.join(map(str, minutes))
+
+print(minutes_str)
+
---
+++ yun-cta-train-status.ino
@@ -0,0 +1,72 @@
+#include <Process.h>
+#include <LiquidCrystal.h>
+#include "constants.h"
+
+LiquidCrystal lcd(LCD_PINS);
+boolean lastButton = LOW;
+boolean currentButton = LOW;
+
+
+void setup() {
+  pinMode(BUTTON_PIN, INPUT);
+  pinMode(BACKLIGHT_PIN, OUTPUT);
+  lcd.begin(16,2);
+  lcd.clear();
+  digitalWrite(BACKLIGHT_PIN, HIGH);
+  lcd.print("Initializing...");
+  Bridge.begin();
+//  Serial.begin(9600);
+  digitalWrite(BACKLIGHT_PIN, LOW);
+  lcd.clear();
+}
+
+void loop() {
+   currentButton = debounce(lastButton);
+   
+   if (lastButton == LOW && currentButton == HIGH) {
+     lcd.clear();
+     lcd.setCursor(0,0);
+     digitalWrite(BACKLIGHT_PIN, HIGH);
+     lcd.print(CTA_STOP_NAME);
+     
+     getTrainTimes();
+     
+     delay(SHOW_RESULTS_DELAY);
+     
+     digitalWrite(BACKLIGHT_PIN, LOW);
+     lcd.clear();
+   }
+   
+   lastButton = currentButton;
+}
+
+boolean debounce(boolean last) {
+   boolean current = digitalRead(BUTTON_PIN);
+   if (last != current) {
+     delay(BUTTON_DEBOUNCE_DELAY);
+     current = digitalRead(BUTTON_PIN);
+   }
+   return current;
+}
+
+void getTrainTimes() {
+  Process p;
+  p.begin("python");
+  p.addParameter(SCRIPT_PATH);
+  p.addParameter(CTA_API_KEY);
+  p.addParameter(CTA_STOP_ID);
+  p.addParameter(DELIMETER);
+  p.run();
+  
+  lcd.setCursor(0,1);
+  
+  while (p.available()>0) {
+    char c = p.read();
+//    Serial.print(c);
+    if (c != '\n') {
+      lcd.write(c);
+    }
+  }
+
+//  Serial.flush();
+}
