b2c38febf556d9ec678f5afe8bf1298863b4cae1
Geoff Buesing <gbuesing@gmail.com>
2014-07-27 22:15:36 -0500

    extract weather values

--- weather.py
+++ weather.py
@@ -1,4 +1,4 @@
-# python weather.py '2379574'
+# python weather.py
 
 # http://weather.yahooapis.com/forecastrss?w=2379574
 
@@ -7,12 +7,20 @@ import urllib2
 import xml.etree.ElementTree as ET
 
 
-WOEID = sys.argv[1] 
+WOEID = '2379574'
 
 def get_xml_from_api():
   api_url = "http://weather.yahooapis.com/forecastrss?w=" + WOEID
   return urllib2.urlopen(api_url).read()
 
 xml = get_xml_from_api()
+# print(xml)
+tree = ET.fromstring(xml)
 
-print(xml)
+item = tree.find("channel/item")
+condition = item.find('yweather:condition', {'yweather': 'http://xml.weather.yahoo.com/ns/rss/1.0'})
+forecast = item.find('yweather:forecast', {'yweather': 'http://xml.weather.yahoo.com/ns/rss/1.0'})
+
+print(condition.attrib['temp'] + ' ' + condition.attrib['text'])
+print('H:' + forecast.attrib['high'] + ' L:' + forecast.attrib['low'])
+
