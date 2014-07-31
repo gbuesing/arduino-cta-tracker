be01824a9d9ad5df5ec0293a52b1637a884a1576
Geoff Buesing <gbuesing@gmail.com>
2014-07-31 16:43:09 -0500

    Modify sketch to use keypad

--- yun-cta-train-status.ino
+++ yun-cta-train-status.ino
@@ -1,74 +1,126 @@
-#include <Process.h>
+#include "Arduino.h"
+#include "Keypad.h"
 #include <LiquidCrystal.h>
+#include <Process.h>
 #include "constants.h"
 
-LiquidCrystal lcd(LCD_PINS);
 
-boolean lastButton = LOW;
-boolean currentButton = LOW;
+LiquidCrystal lcd(A5, A4, A3, A2, A1, A0);
+
+const byte ROWS = 4;
+const byte COLS = 3;
+char keys[ROWS][COLS] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
+};
+byte rowPins[ROWS] = {KEYPAD_ROW_PINS}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {KEYPAD_COL_PINS}; //connect to the column pinouts of the keypad
+
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
 
-void setup() {
-  pinMode(BUTTON_PIN, INPUT);
+void setup(){
   pinMode(LCD_BACKLIGHT_PIN, OUTPUT);
+  lcd.begin(16, 2);
   
-  lcd.begin(16,2);
   digitalWrite(LCD_BACKLIGHT_PIN, HIGH);
   lcd.print(START_MESSAGE);
   
   Bridge.begin();
-//  Serial.begin(9600);
-
+  
   digitalWrite(LCD_BACKLIGHT_PIN, LOW);
   lcd.clear();
 }
-
-void loop() {
-   currentButton = debounce(lastButton);
-   
-   if (lastButton == LOW && currentButton == HIGH) {
-     lcd.clear();
-     digitalWrite(LCD_BACKLIGHT_PIN, HIGH);
-     lcd.print(CTA_STOP_NAME);
-     
-     getTrainTimes();
-     
-     delay(SHOW_RESULTS_DELAY);
-     
-     digitalWrite(LCD_BACKLIGHT_PIN, LOW);
-     lcd.clear();
-   }
-   
-   lastButton = currentButton;
+  
+void loop(){
+  char key = keypad.getKey();
+  
+  if (key){
+    digitalWrite(LCD_BACKLIGHT_PIN, HIGH);
+    
+    String label = getLabel(key);
+    lcd.print(label);
+    
+    String command = getCommand(key);
+    runCommandAndPrint(command);
+    
+    delay(SHOW_RESULTS_DELAY);
+    
+    digitalWrite(LCD_BACKLIGHT_PIN, LOW);
+    lcd.clear();
+  }
 }
 
-boolean debounce(boolean last) {
-   boolean current = digitalRead(BUTTON_PIN);
-   if (last != current) {
-     delay(BUTTON_DEBOUNCE_DELAY);
-     current = digitalRead(BUTTON_PIN);
-   }
-   return current;
-}
-
-void getTrainTimes() {
+void runCommandAndPrint(String command) {
   Process p;
-  p.begin("python");
-  p.addParameter(SCRIPT_PATH);
-  p.addParameter(CTA_API_KEY);
-  p.addParameter(CTA_STOP_ID);
-  p.addParameter(MINUTES_DELIMETER);
-  p.run();
+  p.runShellCommand(command);
   
   lcd.setCursor(0,1);
   
   while (p.available()>0) {
     char c = p.read();
-//    Serial.print(c);
     if (c != '\n') {
       lcd.write(c);
     }
   }
+}
 
-//  Serial.flush();
+String getLabel(char key) {
+  switch ( key ) {
+    case '1':
+      return KEY_1_LABEL;
+    case '2':
+      return KEY_2_LABEL;
+    case '3':
+      return KEY_3_LABEL;
+    case '4':
+      return KEY_4_LABEL;
+    case '5':
+      return KEY_5_LABEL;
+    case '6':
+      return KEY_6_LABEL;
+    case '7':
+      return KEY_7_LABEL;
+    case '8':
+      return KEY_8_LABEL;
+    case '9':
+      return KEY_9_LABEL;
+    case '*':
+      return KEY_STAR_LABEL;
+    case '0':
+      return KEY_0_LABEL;
+    case '#':
+      return KEY_POUND_LABEL;
+  } 
+}
+
+String getCommand(char key) {
+  switch ( key ) {
+    case '1':
+      return KEY_1_COMMAND;
+    case '2':
+      return KEY_2_COMMAND;
+    case '3':
+      return KEY_3_COMMAND;
+    case '4':
+      return KEY_4_COMMAND;
+    case '5':
+      return KEY_5_COMMAND;
+    case '6':
+      return KEY_6_COMMAND;
+    case '7':
+      return KEY_7_COMMAND;
+    case '8':
+      return KEY_8_COMMAND;
+    case '9':
+      return KEY_9_COMMAND;
+    case '*':
+      return KEY_STAR_COMMAND;
+    case '0':
+      return KEY_0_COMMAND;
+    case '#':
+      return KEY_POUND_COMMAND;
+  } 
 }
