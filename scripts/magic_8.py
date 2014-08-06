d37d4dda2a04000f03b3a063d56ea392cc85f247
Geoff Buesing <gbuesing@gmail.com>
2014-08-06 12:10:32 -0500

    Magic 8 ball: update answers

--- scripts/magic_8.py
+++ scripts/magic_8.py
@@ -1,27 +1,15 @@
 import random
 
-
+# https://github.com/arduino/Arduino/blob/master/build/shared/examples/10.StarterKit/p11_CrystalBall/p11_CrystalBall.ino
 answers = """
-It is certain
-It is decidedly so
-Without a doubt
-Yes definitely
-You may rely on it
-As I see it, yes
-Most likely
-Outlook good
 Yes
-Signs point to yes
-Reply hazy try again
-Ask again later
-Better not tell you now
-Cannot predict now
-Concentrate and ask again
-Don't count on it
-My reply is no
-My sources say no
-Outlook not so good
-Very doubtful
+Most likely
+Certainly
+Outlook good
+Unsure
+Ask again
+Doubtful
+No
 """.strip().split("\n")
 
 answer = random.choice(answers)
